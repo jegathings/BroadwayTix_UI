abc87aab28eaacea13b354faa29d012e2ca67a4b
James <jegathings@gmail.com>
2020-07-04 16:30:19 -0400

    Refactored code.

--- src/app.js
+++ src/app.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.scss';
-import Form from './components/Form';
-import ComedyShow from './components/ComedyShow'
+import CreateReservation from './components/CreateReservation';
+import CreateEvent from './components/CreateEvent'
 import Login from './components/Login'
-import ShowResults from './components/CreateShowResults'
+import ShowResultsCreateEvent from './components/ResultsCreateShow'
+import ShowResultsCreateReservation from './components/ResultsCreateReservation';
 
 const App = (props) => {
     const STREET_TEAM_PURCHASE = "https://4o319y7qe2.execute-api.us-east-1.amazonaws.com/dev/post"
     const CREATE_SHOW = "https://4o319y7qe2.execute-api.us-east-1.amazonaws.com/dev/createShow"
     const DO_LOGIN = "https://4o319y7qe2.execute-api.us-east-1.amazonaws.com/dev/login";
-    const [streetTeam, setStreetTeam] = React.useState(false);
-    const [createShow, setCreateShow] = React.useState(false);
+    const SHOW_CREATE_EVENT = "show create an event";
+    const SHOW_CREATE_RESERVATION = "show create reservation";
+    const SHOW_RESULTS_CREATE_RESERVATION = "show results create reservation";
+    const SHOW_RESULTS_CREATE_EVENT = "show results create event";
+    const SHOW_UI = "you're logged in, show ui";
+    const SHOW_LOGIN = "show login";
     const [showLogin, setShowLogin] = React.useState(true);
-    const [createShowResults, setCreateShowResults] = React.useState(null);
-    const [results, setResults] = React.useState(false);
-    
-    const handleStreetTeamPurchase = async (data) => {
-        const response = await fetch(`${STREET_TEAM_PURCHASE}`, {
+    const [showCreateEventPage, setShowCreateEventPage] = React.useState(false);
+    const [showResultsCreateEvent, setShowResultsCreateEvent] = React.useState(false);
+    const [stateCreateEvent,setStateCreateEvent] = React.useState(null);
+    const [stateCreateReservation, setStateCreateReservation] = React.useState(null);
+    const [showCreateReservationPage, setShowCreateReservationPage] = React.useState(false);
+    const [showResultsCreateReservationPage, setShowResultsCreateReservation] = React.useState(false);
+
+    const handeleCreateReservation = async (data) => {
+        await fetch(`${STREET_TEAM_PURCHASE}`, {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json',
             },
             body: JSON.stringify(data),
         })
-            .then(response => response.text())
-            .then(contents => console.log("Contents", contents))
-            .catch((error) => console.log(error))
+        .then(response => response.json())
+        .then(reservation => {
+            setStateCreateReservation(reservation);
+            dispatch(SHOW_RESULTS_CREATE_RESERVATION)})
+        .catch((error) => console.log(error))
     }
 
-    const handleCreateShow = async (data) => {
+    const handleCreateEvent = async (data) => {
         const response = await fetch(`${CREATE_SHOW}`, {
             method: 'POST',
             headers: {
@@ -38,23 +49,21 @@ const App = (props) => {
             body: JSON.stringify(data),
         })
         .then(response => response.json())
+        .then(json => {
+            const event = {id:json.id.S,
+                number_of_tickets: json.number_of_tickets.S,
+                show_comedians: json.show_comedians.S,
+                show_date: json.show_date.S,
+                show_time: json.show_time.S,
+                show_name: json.show_name.S
+                }
+            setStateCreateEvent(event)    
+            dispatch(SHOW_RESULTS_CREATE_EVENT);
+        })
         .catch((error) => console.log(error))
-        console.log("handleCreateShowResponse", response);
-        setResults(true);
-        setShowLogin(false);
-        setStreetTeam(false);
-        setCreateShow(false);
-        setCreateShowResults({id:response.id.S,
-            number_of_tickets: response.number_of_tickets.S,
-            show_comedians: response.show_comedians.S,
-            show_date: response.show_date.S,
-            show_time: response.show_time.S,
-            show_name: response.show_name.S
-            })
     }
 
     const handleLogin = async (data) => {
-        console.log("Removing login token");
         await fetch(`${DO_LOGIN}`, {
             method: 'POST',
             headers: {
@@ -63,12 +72,50 @@ const App = (props) => {
             body: JSON.stringify(data)})        
         .then(response => response.text())
         .then( token => {
-            console.log("The token", token);
             localStorage.setItem("login_token", token);
-            setShowLogin(false);})
+            dispatch(SHOW_UI);})
         .catch((error) => console.log(error));
     }
 
+    const dispatch = (showMe) =>{
+        if(showMe === SHOW_RESULTS_CREATE_RESERVATION){
+            setShowCreateEventPage(false);
+            setShowCreateReservationPage(false);
+            setShowLogin(false);
+            setShowResultsCreateEvent(false);
+            setShowResultsCreateReservation(true);
+        }else if(showMe === SHOW_RESULTS_CREATE_EVENT){
+            setShowCreateEventPage(false);
+            setShowCreateReservationPage(false);
+            setShowLogin(false);
+            setShowResultsCreateReservation(false);
+            setShowResultsCreateEvent(true);
+        }else if(showMe === SHOW_UI){
+            setShowCreateEventPage(false);
+            setShowCreateReservationPage(false);
+            setShowLogin(false);
+            setShowResultsCreateEvent(false);
+            setShowResultsCreateReservation(false);
+        }else if(showMe === SHOW_CREATE_RESERVATION){
+            setShowCreateEventPage(false);
+            setShowLogin(false);
+            setShowResultsCreateEvent(false);
+            setShowResultsCreateReservation(false);
+            setShowCreateReservationPage(true);
+        }else if(showMe === SHOW_CREATE_EVENT){
+            setShowLogin(false);
+            setShowResultsCreateEvent(false);
+            setShowResultsCreateReservation(false);
+            setShowCreateReservationPage(false);
+            setShowCreateEventPage(true);
+        }else if(showMe === SHOW_LOGIN){
+            setShowResultsCreateEvent(false);
+            setShowResultsCreateReservation(false);
+            setShowCreateReservationPage(false);
+            setShowCreateEventPage(false);
+            setShowLogin(true);
+        }
+    }
     return (
         <>
             <h1>Comedy Ticket Hub</h1>
@@ -76,48 +123,36 @@ const App = (props) => {
                 !showLogin &&
                 <nav>
                     <button onClick={() => {
-                        setStreetTeam(true);
-                        setCreateShow(false);
-                        setShowLogin(false);
-                        setCreateShowResults(null);
-                    }}>Street Team</button>
+                        dispatch(SHOW_CREATE_RESERVATION);
+                    }}>Customer Reservation</button>
                     <button onClick={() => {
-                        setCreateShow(true);
-                        setStreetTeam(false);
-                        setShowLogin(false);
-                        setCreateShowResults(null);
-                    }}>Add Show</button>
+                        dispatch(SHOW_CREATE_EVENT);
+                    }}>Create Event</button>
                     <button onClick={() => {
-                        setCreateShow(false);
-                        setStreetTeam(false);
-                        setShowLogin(true);
-                        setCreateShowResults(null);
-                        localStorage.removeItem("login_token");
+                        dispatch(SHOW_LOGIN);
                     }}>Logout</button>
 
                 </nav>
             }
             {
-                showLogin &&
+                showLogin && 
                 <Login formData={{ email: "", password: "" }} handleSubmit={handleLogin} />
             }
             {
-                createShowResults &&
-                <ShowResults formData={{id:createShowResults.id,
-                                        number_of_tickets: createShowResults.number_of_tickets,
-                                        show_comedians: createShowResults.show_comedians,
-                                        show_date: createShowResults.show_date,
-                                        show_time: createShowResults.show_time,
-                                        show_name: createShowResults.show_name
-                                        }}/>
+                showCreateReservationPage && 
+                <CreateReservation formData={{ first_name: "", last_name: "", email: "", broadway_role: "", number_of_tickets: "", show_id: "", formTitle: "Buy Ticket" }} handleSubmit={handeleCreateReservation}/>
             }
             {
-                streetTeam && 
-                <Form formData={{ first_name: "", last_name: "", email: "", broadway_role: "", number_of_tickets: "", show_id: "", formTitle: "Buy Ticket" }} handleSubmit={handleStreetTeamPurchase}></Form>
+                showResultsCreateReservationPage &&
+                <ShowResultsCreateEvent formData={{...stateCreateReservation}}/>
             }
             {
-                createShow && 
-                <ComedyShow formData={{ email: "", number_of_tickets: "", show_name: "", show_date: "", show_time: "", show_room: "", show_comedians: "" }} handleSubmit={handleCreateShow} />
+                showCreateEventPage && 
+                <CreateEvent formData={{ email: "", number_of_tickets: "", show_name: "", show_date: "", show_time: "", show_room: "", show_comedians: "" }} handleSubmit={handleCreateEvent} />
+            }
+            {
+                showResultsCreateEvent &&
+                <ShowResultsCreateEvent formData={{...stateCreateEvent}}/>
             }
         </>
     );
--- src/components/ResultsCreateShow.js
+++ src/components/ResultsCreateShow.js
@@ -4,21 +4,21 @@ export default (props) => {
     const [formData, setFormData] = React.useState(props.formData);
 
     React.useEffect(() => {
+        console.log(props.formData);
         setFormData(props.formData);
-        console.log("Form Data", props.formData);
     }, [props.formData]);
     return (
         <>
             <h1>Receipt Page</h1>
-            <div key={formData.id + "1"}>Show Name</div><br />
+            <div key={formData.id + "1"}><h1>Show Name</h1></div>
             <div key={formData.id + "2"}>{formData.show_name}</div>
-            <div key={formData.id + "3"}>Show Id</div><br />
-            <div key={formData.id + "4"}>{formData.id}</div><br />
-            <div key={formData.id + "5"}>Number Of Tickets</div><br />
+            <div key={formData.id + "3"}><h1>Show Id</h1></div>
+            <div key={formData.id + "4"}>{formData.id}</div>
+            <div key={formData.id + "5"}><h1>Number Of Tickets</h1></div>
             <div key={formData.id + "6"}>{formData.number_of_tickets}</div>
-            <div key={formData.id + "7"}>Show Date</div><br />
+            <div key={formData.id + "7"}><h1>Show Date</h1></div>
             <div key={formData.id + "8"}>{formData.show_date}</div>
-            <div key={formData.id + "9"}>Show Time</div>
+            <div key={formData.id + "9"}><h1>Show Time</h1></div>
             <div key={formData.id + "10"}>{formData.show_time}</div>
         </>
     );
